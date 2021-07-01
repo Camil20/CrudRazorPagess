ca6bdc6d903ac8b5fb0810bc888d9d4009fa04d8
CAMIL EDU DOROTEO MEDINA <cd2018-0268@uce.edu.do>
2021-07-01 15:00:17 -0400

    Master

--- CrudRazorPages/Models/Cliente.cs
+++ CrudRazorPages/Models/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -13,11 +14,18 @@ namespace CrudRazorPages
             Pagos = new HashSet<Pago>();
         }
 
+        [Required(ErrorMessage = "Este campo es requerido")]
         public int ClienteId { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
         public string Nombre { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [StringLength(1)]
         public string Sexo { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [Range(1,2)]
         public int EstadoId { get; set; }
 
+        public virtual Estado Estado { get; set; }
         public virtual ICollection<Factura> Facturas { get; set; }
         public virtual ICollection<Pago> Pagos { get; set; }
     }
